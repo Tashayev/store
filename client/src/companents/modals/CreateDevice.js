5bd3180aae581a8b3bf2231af8d8e8a3991bcf4a
Tashayev <113464233+Tashayev@users.noreply.github.com>
2023-03-28 16:21:48 +0500

    Update CreateDevice.js

--- client/src/companents/modals/CreateDevice.js
+++ client/src/companents/modals/CreateDevice.js
@@ -1,4 +1,4 @@
-//39п. rsc+tab
+
 import React, {useContext, useEffect, useState} from 'react';
 import Modal from "react-bootstrap/Modal";
 import {Button, Dropdown, Form, Row, Col} from "react-bootstrap";
@@ -7,50 +7,46 @@ import {createDevice, fetchBrands, fetchDevices, fetchTypes} from "../../http/de
 import {observer} from "mobx-react-lite";
 
 
-const CreateDevice = observer(({show, onHide}) => {// show - проп отвечает видна ли модальная страница или нет.
-    // onHide - скрывает модальные страницы
+const CreateDevice = observer(({show, onHide}) => {
     const {device} = useContext(Context);
-    const [info, setInfo] = useState([])//41п. useStage заменяет класс.
-    const [price, setPrice] = useState(0)//60п. Для несколько инпутов создаем несколько состоянии
+    const [info, setInfo] = useState([])
+    const [price, setPrice] = useState(0)
     const [name, setName] = useState('')
     const [file, setFile] = useState(null)
     useEffect(()=>{
         fetchTypes().then(data=>device.setTypes(data))
         fetchBrands().then(data=>device.setBrands(data))
     }, [])
-    const addInfo = () =>{// Для кнопки добавить свойства(onClick)
-        setInfo([...info, {title:'', description:'', number: Date.now()}])//
-        // Date.now служить в качестве id (test)
+    const addInfo = () =>{
+        setInfo([...info, {title:'', description:'', number: Date.now()}])
     }
-    const changeInfo = (key, value, number)=>{//60п. Заполнение окон характеристик
+    const changeInfo = (key, value, number)=>{
         setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
-    }//если номер совпадает с номером итерации массива, то разворачиваем(изменяем) в ней объект по ключу(title description),
-    // в ином случае оставляем как есть
-    const removeInfo = (number) =>{//Функция для удаления
+    }
+   
+    const removeInfo = (number) =>{
         setInfo(info.filter(i => i.number !== number))
-        //Удаляем через фильтр если id не соответствует текущему id, то удаляем
-        //number === id в setInfo
+        
     }
-    const selectFile = e => {//60п.
-        setFile(e.target.files[0])//сохраняем файл, по 0-му индексу обращаемся к массиву
+    const selectFile = e => {
+        setFile(e.target.files[0])
     }
-    const addDevice = ()=>{//60п. Отправляем запрос на сервер.
-        const formData = new FormData()//для создания формы девайса используем формдату(как и в постмане)
-        formData.append('name', name)//первый параметр ключ, второй название
+    const addDevice = ()=>{
+        const formData = new FormData()
+        formData.append('name', name)
         formData.append('price',`${price}`)//
         formData.append('img', file)//
-        formData.append('brandId', device.selectedBrand.id)//Брэнд и тип получаем из стора
+        formData.append('brandId', device.selectedBrand.id)
         formData.append('typeId', device.selectedType.id)//
-        formData.append('info', JSON.stringify(info))//массив невозможно передать, по этому передаем как Json строка,
-        // на сервере оно обратно парсится как массив
-        createDevice(formData).then(data=>onHide())//Здесь мы вызываем функцию для запроса на сервер.
-        // Если запрос прошел закрываем
+        formData.append('info', JSON.stringify(info))
+        
+        createDevice(formData).then(data=>onHide())
+        
     }
-    return (// Вставляем из bootstrap modal окна
-        <Modal /*41п. Всплывающие окна*/
+    return (
+        <Modal 
             show={show}
-            onHide={onHide}//Структура content - это массив. Но, когда вы делаете объект observable, то массив преобразовывается
-            // в объект, который уже не является классическим массивом
+            onHide={onHide}
             size="lg"
             centered
         >
@@ -77,7 +73,7 @@ const CreateDevice = observer(({show, onHide}) => {// show - проп отвеч
                     <Dropdown className="mt-3 mb-2">
                         <Dropdown.Toggle>{device.selectedBrand.name || "Выберите тип"}</Dropdown.Toggle>
                         <Dropdown.Menu>
-                            {device.brands.map(brand=>//Так же как и типы получаем брэнды.
+                            {device.brands.map(brand=>
                                 <Dropdown.Item
                                     onClick={()=>device.setSelectedBrand(brand)}
                                     key={brand.id}
@@ -106,7 +102,7 @@ const CreateDevice = observer(({show, onHide}) => {// show - проп отвеч
                     <Form.Control
                         className="mt-3"
                         type="file"
-                        onChange={selectFile}//60п.
+                        onChange={selectFile}
 
                     >
                     </Form.Control>
@@ -117,10 +113,10 @@ const CreateDevice = observer(({show, onHide}) => {// show - проп отвеч
                     >
                         Добавить новые свойства
                     </Button>
-                    {info.map(i=>//Для мобильности передаем массив с инфо в мап
+                    {info.map(i=>
                         <Row className="mt-4" key={i.number}>
                             <Col md={4}>
-                                <Form.Control// Для каждой характеристики отражаем инпуты, кнопки с удалением
+                                <Form.Control
                                 placeholder="Введите название свойства"
                                 value={i.title}
                                 onChange={(e)=>changeInfo("title", e.target.value, i.number )}
@@ -149,7 +145,7 @@ const CreateDevice = observer(({show, onHide}) => {// show - проп отвеч
                 <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
                 <Button variant="outline-success" onClick={addDevice}>Добавить</Button>
             </Modal.Footer>
-        </Modal>/*41п.*/
+        </Modal>
     );
 });
 
